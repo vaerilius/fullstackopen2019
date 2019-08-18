18c64b5c9f8b68377e3ac449f42186559ecabed2
Vaerilius <tjohannest@gmail.com>
2019-08-18 13:17:12 +0300

    1.4: kurssitiedot step4

--- osa 1/kurssitiedot/src/index.js
+++ osa 1/kurssitiedot/src/index.js
@@ -29,6 +29,7 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
+    console.log(props)
     return (
         <>
             <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
@@ -37,25 +38,27 @@ const Total = (props) => {
 };
 
 const App = () => {
-    const course = 'Half Stack application development';
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header course={course}/>
-            <Content parts={[part1, part2, part3]}/>
-            <Total parts={[part1, part2, part3]}/>
+            <Content parts={parts}/>
+            <Total parts={parts}/>
         </div>
     )
 };
