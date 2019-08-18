c07704a98cc23ea24a85d18659c64c6d2c921ad1
Vaerilius <tjohannest@gmail.com>
2019-08-18 11:28:02 +0300

    step1

---
+++ osa 1/kurssitiedot/src/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+        <>
+            <h1> {props.course}</h1>
+        </>
+    )
+};
+const Content = (props) => {
+
+    return (
+
+
+        <>
+            <p > {props.parts[0]} {props.exercices[0]} </p>
+            <p > {props.parts[1]}  {props.exercices[1]}</p>
+            <p > {props.parts[2]}  {props.exercices[2]}</p>
+        </>
+    )
+};
+const Total = (props) => {
+    return (
+        <>
+            <p>Number of exercises {props.exercices[0] + props.exercices[1] + props.exercices[2]}</p>
+        </>
+    )
+};
+
+const App = () => {
+    const course = 'Half Stack application development';
+    const part1 = 'Fundamentals of React';
+    const exercises1 = 10;
+    const part2 = 'Using props to pass data';
+    const exercises2 = 7;
+    const part3 = 'State of a component';
+    const exercises3 = 14;
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={[part1, part2, part3]}  exercices={[exercises1, exercises2, exercises3]}/>
+            <Total  exercices={[exercises1, exercises2, exercises3]}/>
+        </div>
+    )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
