93d4eeeb2b6167a168c8411f293bc26fc5903935
Vaerilius <tjohannest@gmail.com>
2019-08-21 16:33:49 +0300

    1.14*: anekdootit step3

--- osa 1/anekdootit/src/index.js
+++ osa 1/anekdootit/src/index.js
@@ -1,47 +1,63 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+
+    return (
+        <h1> {props.text} </h1>
+    )
+}
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
-    const [votes, setVote] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
+    const [vote, setVote] = useState([0, 0, 0, 0, 0, 0])
+    const copy = [...vote]
 
-    const copy = {...votes}
 
-
-    const testi = () => {
-
+    const voteFor = () => {
         copy[selected] += 1
         setVote(copy)
-        console.log(selected)
-        console.log(copy)
-
     }
-    const testi2 = () => {
 
-        let random = (Math.round(Math.random() * (anecdotes.length - 1)))
-        setSelected(random)
+    const getSelect = () => (setSelected(Math.round(Math.random() * (anecdotes.length - 1))))
+
+    let winner = vote[0]
+    let ane = 0;
+    for (let i = 0; i < vote.length; i++) {
+        if (winner < vote[i]) {
+            winner = vote[i]
+            ane = i;
+        }
     }
 
 
     return (
         <div>
+            <Header text={'Anecdote of the day'}></Header>
             <div>
                 {props.anecdotes[selected]}
                 <br/>
-                {votes[selected]}
+                {vote[selected]}
             </div>
 
             <div>
-                <button onClick={testi}>vote</button>
-                <button onClick={testi2}> next anecdone
+                <button onClick={voteFor}>vote</button>
+                <button onClick={getSelect}> next anecdone
                 </button>
-
+            </div>
+            <div>
+                <Header text={'Anecdote with most votes'}></Header>
+            </div>
+            <div>
+                {props.anecdotes[ane]}
+                <br/>
+                has {vote[ane]} votes
             </div>
 
         </div>
     )
 }
+
 
 const anecdotes = [
     'If it hurts, do it more often',
@@ -51,7 +67,6 @@ const anecdotes = [
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
 
 ReactDOM.render(
     <App anecdotes={anecdotes}/>,
