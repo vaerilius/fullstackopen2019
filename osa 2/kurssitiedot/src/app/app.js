5b83affb583bf5f4c75538fa071e9e594844fc7f
Vaerilius <tjohannest@gmail.com>
2019-08-23 10:10:19 +0300

    2.4: kurssitiedot step9

--- osa 2/kurssitiedot/src/app/app.js
+++ osa 2/kurssitiedot/src/app/app.js
@@ -1,36 +1,61 @@
 import React from 'react'
 import Course from "./course/course";
 
+
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+    const rows = () => courses.map(course =>
+        <Course
+            key={course}
+            header={course.name}
+            parts={course.parts}
+        />
+    )
 
     return (
         <div>
-            <Course course={course} />
+            {rows()}
         </div>
     )
 }
--- osa 2/kurssitiedot/src/app/course/course.js
+++ osa 2/kurssitiedot/src/app/course/course.js
@@ -3,13 +3,13 @@ import Header from "./header/header";
 import Component from "./Content/content";
 import Total from "./Content/total";
 
-const Course = ({course}) => {
-
+const Course = (props) => {
+console.log(props.parts)
     return(
         <div>
-            <Header header={course.name} />
-            <Component parts={course.parts}/>
-            <Total  total={course.parts}/>
+            <Header header={props.header} />
+            <Component parts={props.parts}/>
+            <Total  total={props.parts}/>
 
         </div>
     )
