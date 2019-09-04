03f535307f7a1b7da17325f181139370b5909fc3
Vaerilius <tjohannest@gmail.com>
2019-09-04 11:12:39 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa 4/blogilista/utils/list_helper.js
+++ osa 4/blogilista/utils/list_helper.js
@@ -1,6 +1,13 @@
 const dummy = (blogs) => {
 return 1
 }
+
+const totalLikes = (blogs) => {
+   const reducer = (sum, blog) => sum + blog.likes;
+   return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-   dummy
+   dummy,
+   totalLikes
 }
