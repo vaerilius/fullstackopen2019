ab5d3f3658fdecb1b76638073718b7ee24716568
Vaerilius <tjohannest@gmail.com>
2019-09-04 09:50:04 +0300

    4.2 blogilista, step2

---
+++ osa 4/blogilista/models/blog.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator');
+
+const blogSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    url: {type: String, required: true},
+    likes: Number
+})
+blogSchema.plugin(uniqueValidator)
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
