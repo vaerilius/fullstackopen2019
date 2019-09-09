0610d98c0b3bd2b8352128cebaed9c923f722dd4
Vaerilius <tjohannest@gmail.com>
2019-09-09 11:09:12 +0300

    4.20*: blogilistan laajennus, step9

--- osa 4/blogilista/app.js
+++ osa 4/blogilista/app.js
@@ -25,6 +25,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 
 app.use(bodyParser.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
--- osa 4/blogilista/controllers/blogs.js
+++ osa 4/blogilista/controllers/blogs.js
@@ -4,13 +4,6 @@ const User = require('../models/user')
 
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
 
 blogsRouter.get('/', async (request, response, next) => {
     const blogs = await Blog.find({})
@@ -39,20 +32,17 @@ blogsRouter.post('/', async (request, response, next) => {
 
     const body = request.body
 
-    const token = getTokenFrom(request)
-
-
     try {
-
-        const decodedToken = jwt.verify(token, process.env.SECRET)
-        
-        if (!token || !decodedToken.id) {
+        console.log('user');
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+       
+        if (!request.token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
 
         const user = await User.findById(decodedToken.id)
 
-
+            
         const blog = new Blog({
             title: body.title,
             author: body.author,
@@ -61,8 +51,6 @@ blogsRouter.post('/', async (request, response, next) => {
             user: user._id
         })
 
-
-
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
@@ -70,13 +58,6 @@ blogsRouter.post('/', async (request, response, next) => {
     } catch (error) {
         next(error)
     }
-
-    // blog
-    //     .save()
-    //     .then(result => {
-    //         response.status(201).json(result)
-    //     })
-    //     .catch(err => next(err))
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
