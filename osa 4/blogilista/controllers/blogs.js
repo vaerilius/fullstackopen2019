590cc80540f60606121887d0857233213a657e0e
Vaerilius <tjohannest@gmail.com>
2019-09-09 11:49:07 +0300

    4.21*: blogilistan laajennus, step10

--- osa 4/blogilista/controllers/blogs.js
+++ osa 4/blogilista/controllers/blogs.js
@@ -33,16 +33,15 @@ blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
     try {
-        console.log('user');
         const decodedToken = jwt.verify(request.token, process.env.SECRET)
-       
+
         if (!request.token || !decodedToken.id) {
             return response.status(401).json({ error: 'token missing or invalid' })
         }
 
         const user = await User.findById(decodedToken.id)
 
-            
+
         const blog = new Blog({
             title: body.title,
             author: body.author,
@@ -61,10 +60,20 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-
     try {
-        await Blog.findByIdAndDelete(request.params.id)
-        response.status(204).end()
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        const user = await User.findById(decodedToken.id)
+        const blog = await Blog.findById(request.params.id)
+
+        if (!request.token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        } else if (blog.user.toString() !== user._id.toString()) {
+            return response.status(401).json({ error: 'wrong token' })
+        } else if (blog.user.toString() === user._id.toString()) {
+            await Blog.deleteOne({ _id: request.params.id })
+            response.status(204).end()
+        } 
+
     } catch (error) {
         next(error)
     }
