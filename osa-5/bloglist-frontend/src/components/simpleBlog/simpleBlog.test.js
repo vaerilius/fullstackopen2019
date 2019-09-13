161206517a545e2f8807fd3ed99b12592eb29c05
Vaerilius <tjohannest@gmail.com>
2019-09-13 12:47:17 +0300

    5.14: blogilistan testit, step2

--- osa-5/bloglist-frontend/src/components/simpleBlog/simpleBlog.test.js
+++ osa-5/bloglist-frontend/src/components/simpleBlog/simpleBlog.test.js
@@ -2,9 +2,12 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, cleanup } from '@testing-library/react'
 import SimpleBlog from './simple-blog'
-import { prettyDOM } from '@testing-library/dom'
+import { prettyDOM, fireEvent } from '@testing-library/dom'
+import { async } from 'q'
 
 afterEach(cleanup)
+
+
 
 test('renders content ', () => {
   const blog = {
@@ -24,9 +27,29 @@ test('renders content ', () => {
   expect(component.container).toHaveTextContent(
     'blog has 1 likes'
   )
-
-
   console.log(prettyDOM)
+
+})
+test('clicking the button calls event handler two times', async () => {
+
+  const blog = {
+    title: 'this is title',
+    author: 'facebook jest',
+    likes: 1
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 
 })
 
