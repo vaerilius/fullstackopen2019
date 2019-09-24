1c319936e578ee55b456a99c3a8fe710952505e9
vaerilius <vaerilius@gmail.com>
2019-09-24 13:09:04 +0300

    uibuild

---
+++ osa-7/bloglist/blogilista/build/precache-manifest.ecf0d34a239ed4d069934086176c58f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3916a364c01d3f22caf3",
+    "url": "/static/js/main.3916a364.chunk.js"
+  },
+  {
+    "revision": "1124378225cdadd9499c",
+    "url": "/static/js/1.11243782.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09fdb9c316d50bb49aca131db043ce41",
+    "url": "/index.html"
+  }
+];
