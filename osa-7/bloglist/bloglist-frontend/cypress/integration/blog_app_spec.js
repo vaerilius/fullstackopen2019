b17baf2e300d5518f2603ce804c5938ad48f2596
vaerilius <vaerilius@gmail.com>
2019-09-24 11:54:24 +0300

    7.18 End to end -testaus, step2

--- osa-7/bloglist/bloglist-frontend/cypress/integration/blog_app_spec.js
+++ osa-7/bloglist/bloglist-frontend/cypress/integration/blog_app_spec.js
@@ -1,25 +1,42 @@
-describe('Blog App ', function() {
-    beforeEach(function() {
-        cy.visit('http://localhost:3000')
-        cy.get('#username')
-        .type('aaa')
-        cy.get('#password')
-        .type('aaaa')
-        cy.contains('login')
-        .click()
-      })
-    it('User can login', function() {
-        cy.contains('aaaa logged in')
-    })
-    it('a new blog can be created', function() {
-       cy.contains('create new')
-       .click()
-       cy.get('#title').type('test blog from cypress')
-       cy.get('#author').type('cypress')
-       cy.get('#url')
-       .type('https://docs.cypress.io/guides/references/best-practices.html')
-       cy.get('#create').click()
+describe('Blog App ', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
 
-       cy.contains('test blog from cypress cypress')
+    const user = {
+      name: 'timo testaaja',
+      username: 'testaaja',
+      password: 'test'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+
+
+  })
+  describe('when logged in', function () {
+
+    beforeEach(function () {
+      cy.get('#username')
+        .type('testaaja')
+      cy.get('#password')
+        .type('test')
+      cy.contains('login')
+        .click()
+    })
+
+    it('User can login', function () {
+      cy.contains('timo testaaja logged in')
+    })
+    it('a new blog can be created', function () {
+      cy.contains('create new')
+        .click()
+      cy.get('#title').type('test blog from cypress')
+      cy.get('#author').type('cypress')
+      cy.get('#url')
+        .type('https://docs.cypress.io/guides/references/best-practices.html')
+      cy.get('#create').click()
+
+      cy.contains('test blog from cypress cypress')
     })
   })
+
+})
